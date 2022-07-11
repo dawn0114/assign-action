0c3b47465141afcdc6b0328f5614c440e8259ed3
dawn <dy.lee@bucketplace.net>
2022-07-11 15:25:17 +0900

    provide fallback in place

--- src/utils.ts
+++ src/utils.ts
@@ -117,7 +117,7 @@ export function chooseUsersFromGroups(
       }
     } else {
       for (const label of labels) {
-        if (groups.keys.indexOf(label) > -1) {
+        if ((groups.keys || []).includes(label)) {
           users = users.concat(chooseUsers(groups[label], desiredNumber, owner))
         }
       }
